66252bdf58347b9366933fa0a4636a2a3db3df2e
KPerson007 <KPerson007@users.noreply.github.com>
2016-02-04 22:43:26 -0800

    Add all files

---
+++ Simon/Main.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Simon
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.timerAssign = new System.Windows.Forms.Timer(this.components);
+            this.pictureBoxGame = new System.Windows.Forms.PictureBox();
+            this.timerRead = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGame)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start Game";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Location = new System.Drawing.Point(93, 17);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(47, 13);
+            this.labelScore.TabIndex = 5;
+            this.labelScore.Text = "Score: 0";
+            // 
+            // timerAssign
+            // 
+            this.timerAssign.Interval = 750;
+            this.timerAssign.Tick += new System.EventHandler(this.timerAssign_Tick);
+            // 
+            // pictureBoxGame
+            // 
+            this.pictureBoxGame.Image = global::Simon.Properties.Resources.Simon;
+            this.pictureBoxGame.Location = new System.Drawing.Point(15, 41);
+            this.pictureBoxGame.Name = "pictureBoxGame";
+            this.pictureBoxGame.Size = new System.Drawing.Size(150, 150);
+            this.pictureBoxGame.TabIndex = 6;
+            this.pictureBoxGame.TabStop = false;
+            this.pictureBoxGame.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxGame_MouseDown);
+            this.pictureBoxGame.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBoxGame_MouseUp);
+            // 
+            // timerRead
+            // 
+            this.timerRead.Interval = 1000;
+            this.timerRead.Tick += new System.EventHandler(this.timerRead_Tick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(180, 207);
+            this.Controls.Add(this.pictureBoxGame);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.buttonStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simon";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGame)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Timer timerAssign;
+        private System.Windows.Forms.PictureBox pictureBoxGame;
+        private System.Windows.Forms.Timer timerRead;
+    }
+}
+
---
+++ Simon/Main.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Simon
+{
+    public partial class Main : Form
+    {
+        int GameState = 1;
+        List<int> SimonSays = new List<int>();
+        int LastClickIndex = 0;
+        int Score = 0;
+        int CurrentIndex = 0;
+        Random myrandomgen = new Random();
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        void ResetGame()
+        {
+            GameState = 4;
+            timerAssign.Enabled = false;
+            buttonStart.Enabled = false;
+            LastClickIndex = 0;
+            CurrentIndex = 0;
+            buttonStart.Text = "Start Game";
+            UpdateScore(0);
+            Console.WriteLine("Loss!");
+            SimonSays.Clear();
+            SetColor("All", "0");
+            timerRead.Enabled = true;
+        }
+
+        void SetColor(string Color, string Sound)
+        {
+            Console.WriteLine(Color);
+            pictureBoxGame.Image = (Image)Simon.Properties.Resources.ResourceManager.GetObject(Color + "_Simon");
+        }
+
+        int RandomGenerator()
+        {
+            return myrandomgen.Next(0, 4);
+        }
+
+        void AssignRead()
+        {
+            CurrentIndex = 0;
+            SimonSays.Add(RandomGenerator());
+            Console.WriteLine("Colors:");
+            timerAssign.Enabled = true;
+        }
+
+        void UpdateScore(int NewScore)
+        {
+            Score = NewScore;
+            labelScore.Text = "Score: " + Score.ToString();
+        }
+
+        void CheckResponse(int id)
+        {
+            if (SimonSays.Count > 0)
+            {
+                if (SimonSays.ElementAt(LastClickIndex) == id)
+                {
+                    LastClickIndex++;
+                    if (LastClickIndex == SimonSays.Count)
+                    {
+                        LastClickIndex = 0;
+                        UpdateScore(Score + 1);
+                        Console.WriteLine("Win!");
+                        GameState = 2;
+                        AssignRead();
+                    }
+                }
+                else
+                {
+                    ResetGame();
+                }
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (GameState == 1)
+            {
+                GameState = 2;
+                buttonStart.Text = "Stop Game";
+                AssignRead();
+            }
+            else if (GameState == 4)
+            {
+            }
+            else
+            {
+                ResetGame();
+            }
+        }
+
+        private void timerAssign_Tick(object sender, EventArgs e)
+        {
+            pictureBoxGame.Image = Simon.Properties.Resources.Simon;
+            if (CurrentIndex == SimonSays.Count)
+            {
+                pictureBoxGame.Image = Simon.Properties.Resources.Simon;
+                CurrentIndex = 0;
+                timerAssign.Enabled = false;
+                GameState = 3;
+            }
+            else
+            {
+                switch (SimonSays.ElementAt(CurrentIndex))
+                {
+                    case 0:
+                        SetColor("Green", "0");
+                        break;
+                    case 1:
+                        SetColor("Red", "0");
+                        break;
+                    case 2:
+                        SetColor("Yellow", "0");
+                        break;
+                    case 3:
+                        SetColor("Blue", "0");
+                        break;
+                }
+                CurrentIndex++;
+            }
+        }
+
+        private void timerRead_Tick(object sender, EventArgs e)
+        {
+            pictureBoxGame.Image = Simon.Properties.Resources.Simon;
+            GameState = 1;
+            buttonStart.Enabled = true;
+            timerRead.Enabled = false;
+        }
+
+        private void pictureBoxGame_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (GameState == 3)
+            {
+                timerRead.Enabled = false;
+                pictureBoxGame.Image = Simon.Properties.Resources.Simon;
+                Point relativePoint = this.PointToClient(Cursor.Position);
+                Console.WriteLine(relativePoint.ToString());
+                if (relativePoint.X >= 15 && relativePoint.X < 90) //either green or blue
+                {
+                    Console.WriteLine("either green or blue");
+                    if (relativePoint.Y >= 41 && relativePoint.Y < 116) //green
+                    {
+                        SetColor("Green", "0");
+                        CheckResponse(0);
+                    }
+                    else if (relativePoint.Y >= 116 && relativePoint.Y <= 191) //blue
+                    {
+                        SetColor("Blue", "0");
+                        CheckResponse(3);
+                    }
+                }
+                else if (relativePoint.X >= 90 && relativePoint.X <= 165) //either red or yellow
+                {
+                    Console.WriteLine("either red or yellow");
+                    if (relativePoint.Y >= 41 && relativePoint.Y < 116) //red
+                    {
+                        SetColor("Red", "0");
+                        CheckResponse(1);
+                    }
+                    else if (relativePoint.Y >= 116 && relativePoint.Y <= 191) //yellow
+                    {
+                        SetColor("Yellow", "0");
+                        CheckResponse(2);
+                    }
+                }
+            }
+        }
+
+        private void pictureBoxGame_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (!(GameState == 4))
+            {
+                pictureBoxGame.Image = Simon.Properties.Resources.Simon;
+            }
+        }
+    }
+}
